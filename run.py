d1d84c7c25bc2cae5ee10860042847a4f94b67f2
naibor <liznaibor.en@gmail.com>
2018-05-31 12:33:44 +0300

    [Feature #157968642]add imports

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from app.api.api import app 
 # flask instance
+from instance.config import app_config
+# enviromnent configurations setting
+
+
+if __name__ =="__main__":
+    app.config.from_object(app_config['development'])
+    app.run
