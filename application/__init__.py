a2061c6a019a0b869996e64abbfb22953ad90af7
Isaac Ndung'u <inwainaina93@gmail.com>
2018-06-11 15:21:30 +0300

    fix route/url addition for user registration resource.

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 from flask_restful import Api
 
-app = Flask("application")
+app = Flask(__name__, instance_relative_config=True)
 
-api = Api(app)
+api = Api()
+
+from application.resource.user_resource import RegisterApi
+
+# add_resource function registers the routes with the framework using given endpoints
+api.add_resource(RegisterApi,'/api/v1/auth/register')
+
+api.init_app(app)
+
